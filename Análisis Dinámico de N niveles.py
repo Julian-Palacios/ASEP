77d5c9458cb3259442cae16a20a887462d0fd6bc
Julian Palacios <jpalaciose@uni.pe>
2021-06-13 22:31:15 -0500

    Main Codes Added

---
+++ Análisis Dinámico de N niveles.py
@@ -0,0 +1,318 @@
+# Código realizado por JPI Ingeniería e Innovación SAC
+# Realizado para el Curso:
+# "Análisis Sísmico de Edificaciones en Python"
+
+import openseespy.opensees as ops
+import openseespy.postprocessing.ops_vis as opsv
+import openseespy.postprocessing.Get_Rendering as opsplt
+import matplotlib.pyplot as plt
+from OSP_tools import *
+import numpy as np
+import pandas as pd
+
+# Creamos el modelo
+ops.wipe()
+ops.model('basic', '-ndm', 3, '-ndf', 6)
+
+# Generamos la malla
+RigidDiaphragm = 'ON'
+dx, dy, alt  = 4, 4, 3 # m
+nx, ny, nz = 3, 2, 3
+if nx*dx>ny*dy:
+    limx1 = 0
+    limx2 = nx*dx
+    limy1 = (ny*dy-limx2)/2
+    limy2 = limx2 + limy1
+else:
+    limy1 = 0
+    limy2 = ny*dy
+    limx1 = (nx*dx-limy2)/2
+    limx2 = limy2 + limx1
+
+Nodes, Elems, Diap = GeoModel(dx,dy,alt,nx,ny,nz)
+
+# Asignamos los nodos
+for Ni in Nodes:
+    ops.node(int(Ni[0]), *Ni[1:])
+
+# Definimos diafragma rígidos
+if RigidDiaphragm == 'ON':
+    dirDia = 3 # perpendicular al plano del diafragma
+    for Nd in Diap:
+        # Nd[2]=Nd[2]+dy*ny*0.05 # excentricidad para analisis en X
+        ops.node(int(Nd[0]), *Nd[1:]*m)
+        ops.fix(int(Nd[0]),*[0,0,1,1,1,0])
+        NodesDi = []
+        for Ni in Nodes:
+            if Ni[3]==Nd[3]:
+                NodesDi.append(int(Ni[0]))
+        ops.rigidDiaphragm(dirDia,int(Nd[0]),*NodesDi)
+
+# Restricciones
+ops.fixZ(0.0, *[1,1,1,1,1,1], '-tol', 1e-10)
+
+# Definimos la sección
+ops.section('Elastic', 1, E, Ac, Izc, Iyc, G, Jxxc)
+ops.section('Elastic', 2, E, A, Iz, Iy, G, Jxx)
+Nint = 5 #Number of integration points
+ops.beamIntegration('Lobatto', 1, 1, Nint)
+ops.beamIntegration('Lobatto', 2, 2, Nint)
+CTransf = 1
+BTransf = 2
+ops.geomTransf('PDelta', CTransf, 1, 0, 0)
+ops.geomTransf('Linear', BTransf, 0, 0, 1)
+
+# Creamos los elementos
+for Ele in Elems:
+    if int(Ele[3]) == 1:# Column
+        ops.element('forceBeamColumn', int(Ele[0]), int(Ele[1]), int(Ele[2]), int(Ele[3]), 1, '-mass', ρlc)
+    else: # Beam
+        ops.element('forceBeamColumn', int(Ele[0]), int(Ele[1]), int(Ele[2]), int(Ele[3]), 2, '-mass', ρl*(dx-a)/dx)
+
+opsv.plot_model(fig_wi_he=(30., 25.),az_el=(-50,10),fig_lbrt = (0, 0, 1, 1))
+plt.axis([limx1,limx2,limy1,limy2])
+plt.savefig('./Modelo_numerico.png',dpi=200)
+
+# Aplicando Cargas vivas y muertas
+wLive = 250*kg/m**2
+wLosa = 300*kg/m**2
+wAcab = 100*kg/m**2
+wTabi = 150*kg/m**2
+wTotal = 1.0*(wLosa+wAcab+wTabi)+0.25*wLive
+#
+Carga = wTotal*dx*dy*m**2
+for Ni in Nodes:
+    ops.mass(int(Ni[0]),Ni[4]*Carga,Ni[4]*Carga,0.0)
+# print('Masa en el Nodo 36:',ops.nodeMass(36))
+
+# ploteamos el modelo
+ele_shapes = {}
+#
+for i in range(int(Ele[0])+1):
+    if int(Elems[i,3])==1: # Column
+        ele_shapes[i] = ['rect', [a, a]]
+    else: # Beam
+        ele_shapes[i] = ['rect', [b, h]]
+
+opsv.plot_extruded_shapes_3d(ele_shapes, fig_wi_he=(30.0, 25.0), 
+                        az_el=(-50,10),fig_lbrt = (0, 0, 1, 1))
+plt.axis([limx1,limx2,limy1,limy2])
+# plt.show()
+plt.savefig('./Modelo_3D.png',dpi=200)
+
+# Obtenemos los modos
+Nmodes = 12
+Tmodes = ops.eigen(Nmodes)
+
+# ploteamos el modo 1
+opsplt.plot_modeshape(1, 500)
+plt.savefig('./Modo_1.png',dpi=200) # comentar plt.show() de código fuente
+opsplt.plot_modeshape(2, 500)
+plt.savefig('./Modo_2.png',dpi=200) 
+opsplt.plot_modeshape(3, 500)
+plt.savefig('./Modo_3.png',dpi=200)
+
+Tmodes = ops.eigen(Nmodes)
+for i in range(Nmodes):
+    Tmodes[i] = 2*np.pi/Tmodes[i]**0.5
+
+# guardando las formas modales
+NT = 3*nz
+Tags = ops.getNodeTags()
+modo = np.zeros((Nmodes,NT))
+for j in range(1,Nmodes+1):
+    ind = 0
+    for i in Tags[-nz:]:
+        temp = ops.nodeEigenvector(i,j)
+        modo[j-1,[ind,ind+1,ind+2]] = temp[0],temp[1],temp[-1]
+        ind = ind + 3
+
+# Realizamos un análisis para obtener la matriz de Masas
+ops.system('FullGeneral')
+ops.numberer("Plain")
+ops.constraints('Transformation') 
+ops.algorithm('Linear')
+ops.analysis('Transient')
+ops.integrator('GimmeMCK',1.0,0.0,0.0)
+ops.analyze(1,0.0) 
+
+# Obtenemos la matriz de Masas
+N = ops.systemSize()         # Número de Grados de Libertad
+Mmatrix = ops.printA('-ret')
+Mmatrix = np.array(Mmatrix)
+Mmatrix.shape = (N,N)
+MF = Mmatrix[N-NT:,N-NT:]
+
+# Obtenemos las pseudo aceleraciones
+Ro=8.
+E030 = espectro_E030(Tmodes,Z=0.45,U=1.0,S=1.0,Tp=0.4,Tl=2.5,R=Ro)
+#
+x = np.arange(0,3.0,0.02)
+y = espectro_E030(x,Z=0.45,U=1.0,S=1.0,Tp=0.4,Tl=2.5,R=Ro)
+plt.close('all')
+plt.plot(x,y,'k-')
+plt.xlabel('Periodo (s)')
+plt.ylabel('Sa (g)')
+plt.axis([0,3.0,0,1.1*y.max()])
+plt.savefig('Espectro_E030.png')
+#
+H = np.arange(1,nz+1)*alt
+P = sum(MF[0::3,0::3]) # Masa por nivel
+print('Masa por Nivel:\n',P)
+#
+DDx, ΔDx, VDx, DDy, ΔDy, VDy, df1, df2 = getCombo(E030,MF,modo,NT,Tmodes)
+print('\nFACTOR DE MASAS PARTICIPATIVAS')
+print(df1.round(4))
+#
+print('\nANÁLISIS DINÁMICO SIN ESCALAR')
+print(df2.round(4))
+
+# Obteniendo fuerzas estáticas
+F, k = get_static_loads(E030[0],P,H,Tmodes[0])
+texto1 = 'Las fuerzas distribuidas en altura del análisis estático se \
+obtinen a partir del coeficiente basal (%.4f) y el exponente k \
+relacionado al periodo fundamental (%.4f). '%(E030[0],k)
+texto1 = texto1 + 'A continuación, se muestran las fuerzas \
+y los desplazamientos obtenidos del análisis estático en la \
+dirección X e Y.'
+
+# Definición de caso de carga estático
+tsTag = 1
+ops.timeSeries('Linear', tsTag, '-factor', 1.0)
+ops.pattern('Plain', 1, tsTag, '-fact', 1.0)
+
+# Aplicando fuerzas estáticas en X
+Le = ny*dy*0.05
+for i in range(nz):
+    ops.load(int(Diap[i][0]),F[i]*9.81,0.,0.,0.,0.,F[i]*9.81*Le)
+
+# Ejecución del análisis estático
+ops.wipeAnalysis()
+ops.system('FullGeneral')
+ops.numberer('Plain')
+ops.constraints('Transformation') 
+ops.integrator('LoadControl',1)
+ops.algorithm('Linear')
+ops.analysis('Static')
+ops.analyze(1)
+ops.loadConst('-time', 0.0)	# para reestablecer el tiempo a 0s
+#
+VS = np.cumsum(F[::-1])[::-1]
+print('\nANÁLISIS ESTÁTICO EN X')
+df3 = pd.DataFrame(columns=['Nivel','Vx(ton)','UxMax(cm)','UyMax(cm)','DriftX(‰)','DriftY(‰)'])
+tempX, tempY = 0., 0.
+for i in range(nz):
+    desX = ops.nodeDisp(int(Diap[i][0]))[0]
+    desY = ops.nodeDisp(int(Diap[i][0]))[1]
+    rotZ = ops.nodeDisp(int(Diap[i][0]))[5]
+    desX = (desX + abs(rotZ*ny*dy/2))*0.75*Ro
+    desY = (desY + abs(rotZ*nx*dx/2))*0.75*Ro
+    driftX=1000.*(desX-tempX)/alt
+    driftY=1000.*(desY-tempY)/alt 
+    tempX, tempY = desX, desY
+    df3 = df3.append({'Nivel':i+1,'Vx(ton)':VS[i]/1000,'UxMax(cm)':desX*100,'UyMax(cm)':desY*100,
+                    'DriftX(‰)':driftX,'DriftY(‰)':driftY}, ignore_index=True)
+print(df3.round(4))
+#
+# sfac = 100.0
+# fig_wi_he = 30., 20.
+# nep = 9
+# opsv.plot_defo(sfac, nep, fmt_interp='b-', az_el=(45., 20.),
+#                fig_wi_he=fig_wi_he, endDispFlag=0)
+# plt.title('3d 3-element cantilever beam')
+# plt.show()
+
+# Definición de caso de carga estático
+ops.remove('timeSeries',1)
+ops.remove('loadPattern',1)
+tsTag = 1
+ops.timeSeries('Linear', tsTag, '-factor', 1.0)
+ops.pattern('Plain', 1, tsTag, '-fact', 1.0)
+
+# Aplicando fuerzas estáticas en Y
+Le = nx*dx*0.05
+for i in range(nz):
+    ops.load(int(Diap[i][0]),0.,F[i]*9.81,0.,0.,0.,F[i]*9.81*Le)
+
+# Ejecución del análisis estático
+ops.wipeAnalysis()
+ops.system('FullGeneral')
+ops.numberer('Plain')
+ops.constraints('Transformation') 
+ops.integrator('LoadControl',1)
+ops.algorithm('Linear')
+ops.analysis('Static')
+ops.analyze(1)
+#
+VS = np.cumsum(F[::-1])[::-1]
+print('\nANÁLISIS ESTÁTICO EN Y')
+df4 = pd.DataFrame(columns=['Nivel','Vy(ton)','UxMax(cm)','UyMax(cm)','DriftX(‰)','DriftY(‰)'])
+tempX, tempY = 0., 0.
+for i in range(nz):
+    desX = ops.nodeDisp(int(Diap[i][0]))[0]
+    desY = ops.nodeDisp(int(Diap[i][0]))[1]
+    rotZ = ops.nodeDisp(int(Diap[i][0]))[5]
+    desX = (desX + abs(rotZ*ny*dy/2))*0.75*Ro
+    desY = (desY + abs(rotZ*nx*dx/2))*0.75*Ro
+    driftX=1000.*(desX-tempX)/alt
+    driftY=1000.*(desY-tempY)/alt 
+    tempX, tempY = desX, desY
+    df4 = df4.append({'Nivel':i+1,'Vy(ton)':VS[i]/1000,'UxMax(cm)':desX*100,'UyMax(cm)':desY*100,
+                    'DriftX(‰)':driftX,'DriftY(‰)':driftY}, ignore_index=True)
+print(df4.round(4))
+
+# Escalamiento de los resultados del análisis dinámico
+if VDx[0::3][0]<0.80*VS[0]:
+    FSx  = 0.90*VS[0]/VDx[0::3][0]
+    msjx = 'SI es necesario aplicar un Factor de Escala en X: %.4f'%FSx
+else:
+    FSx = 1.
+    msjx = 'NO es necesario escalar en X'
+
+if VDy[0::3][0]<0.80*VS[0]:
+    FSy  = 0.90*VS[0]/VDy[0::3][0]
+    msjy = 'SI es necesario aplicar un Factor de Escala en Y: %.4f'%FSy
+else:
+    FSy = 1.
+    msjy = 'NO es necesario escalar en Y'
+
+texto2 = '\nAl comparar la cortante basal obtenida en el análisis dinámico en X \
+(%.2f ton) y el 80%% de la cortante basal del análisis estático en X (%.2f ton), \
+se obtiene que %s. '%(VDx[0::3][0]/1000,0.80*VS[0]/1000,msjx)
+texto2 = texto2 + 'En la dirección Y, la cortante basal obtenida en el análisis \
+dinámico es %.2f ton y el 80%% de la cortante basal del análisis estático es %.2f ton. \
+Por lo que %s.'%(VDy[0::3][0]/1000,0.80*VS[0]/1000,msjy)
+
+# Se aplican los Factores de Escala
+print('\nANÁLISIS DINÁMICO FINAL')
+df5 = pd.DataFrame(columns=['Nivel','Vx(ton)','Vy(ton)','Ux(cm)','Uy(cm)','Δx(‰)','Δy(‰)'])
+for i in range(nz):
+    Δx = FSx*0.75*Ro*ΔDx[0::3][i]/alt
+    Δy = FSy*0.75*Ro*ΔDy[1::3][i]/alt
+    #
+    df5 = df5.append({'Nivel':i+1, 'Vx(ton)':FSx*VDx[0::3][i]/1000,
+        'Vy(ton)':FSy*VDy[1::3][i]/1000,'Ux(cm)':FSx*0.75*Ro*DDx[0::3][i]*100,
+        'Uy(cm)':FSy*0.75*Ro*DDy[1::3][i]*100,'Δx(‰)':Δx*1000,'Δy(‰)':Δy*1000}, ignore_index=True)
+df5 = df5.astype({'Nivel':int})
+print(df5.round(4))
+# sfac = 100.0
+# fig_wi_he = 30., 20.
+# nep = 9
+# opsv.plot_defo(sfac, nep, fmt_interp='b-', az_el=(45., 20.),
+#                fig_wi_he=fig_wi_he, endDispFlag=0)
+# plt.title('3d 3-element cantilever beam')
+# plt.show()
+vecX = np.array(df5.loc[:,'Δx(‰)'])
+vecY = np.array(df5.loc[:,'Δy(‰)'])
+lim = 1.1*max(vecX.max(),vecY.max())
+plt.close('all')
+plt.plot(np.insert(vecX,0,0),np.arange(nz+1),'bo--',label='drift X',lw = 0.8)
+plt.plot(np.insert(vecY,0,0),np.arange(nz+1),'ro--',label='drift Y',lw = 0.8)
+plt.legend()
+plt.xlabel('Distorsión (‰)')
+plt.ylabel('Nivel')
+plt.axis([-0.05,lim,-0.05,nz+0.05])
+plt.yticks(np.arange(0, nz+0.05, 1))
+plt.savefig('distorsion_din.png')
+
+genReport(df1,df2.iloc[::-1],df3.iloc[::-1],df4.iloc[::-1],df5.iloc[::-1],texto1,texto2)
---
+++ OSP_tools.py
@@ -0,0 +1,402 @@
+#--------------------------------------------------------
+#   LibUnits.py - Define system of units OpenSees model
+#   Code:      II
+#--------------------------------------------------------
+# base units (SI units)
+m = 1
+kg = 1
+s = 1
+# other units
+N = kg*m/s**2
+cm = 0.01*m
+Pa = N/m**2
+ksi = 6894757.2932*Pa
+kgf = 9.8066*N
+MPa = 10**6*Pa
+psi = 6894.76*Pa
+# physical constants
+g = 9.81*m/s**2
+
+# Propiedades de los materiales
+fc = 210 # kg/cm2
+E = 15100*fc**0.5*10**4*9.81*Pa
+G = 0.5*E/(1+0.2)
+# Sección de Columna
+a = 50*cm
+Ac = a**2
+ρlc = 2400*Ac*m**2
+Izc = a**4/12
+Iyc = a**4/12
+Jxxc = 2.25*(a/2)**4
+# Sección de Viga
+b = 50*cm
+h = 25*cm
+A = b*h
+ρl = 2400*A*m**2
+Iz = b*h**3/12
+Iy = b**3*h/12
+Jxx = 0.229*max(b,h)*min(b,h)**3 # modificar
+#
+def GeoModel(dx, dy, h, nx, ny, nz):
+    from numpy import zeros, ones
+    import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
+    Lx, Ly, Lz = dx*nx, dy*ny, h*nz
+    NN = (nx+1)*(ny+1)*(nz+1)
+    Nodes = zeros((NN,5))
+    # Creando los nodos y asignando coordenadas
+    c = 0
+    for i in range(nz+1):
+        for j in range(ny+1):
+            for k in range(nx+1):
+                if k == nx and j != ny and j!= 0:
+                    Nodes[c] = [c,k*dx,j*dy,i*h,0.50]
+                elif k != nx and j == ny  and k!= 0:
+                    Nodes[c] = [c,k*dx,j*dy,i*h,0.50]
+                elif k == 0 and j != ny and j!= 0:
+                    Nodes[c] = [c,k*dx,j*dy,i*h,0.50]
+                elif k != nx and j == 0  and k!= 0:
+                    Nodes[c] = [c,k*dx,j*dy,i*h,0.50]
+                elif k == nx and j == ny:
+                    Nodes[c] = [c,k*dx,j*dy,i*h,0.25]
+                elif k == 0 and j == 0:
+                    Nodes[c] = [c,k*dx,j*dy,i*h,0.25]
+                elif k == nx and j == 0:
+                    Nodes[c] = [c,k*dx,j*dy,i*h,0.25]
+                elif k == 0 and j == ny:
+                    Nodes[c] = [c,k*dx,j*dy,i*h,0.25]
+                else:
+                    Nodes[c] = [c,k*dx,j*dy,i*h,1.00]
+                c = c + 1
+    Nodes[:(nx+1)*(ny+1),4]=0
+    # print(Nodes)
+
+    #
+    # fig = plt.figure(figsize=(20, 10))
+    # ax = fig.add_subplot(111, projection='3d')
+    # from mpl_toolkits.mplot3d import Axes3D
+
+    # fig = plt.figure()
+    # ax1 = fig.add_subplot(111, projection='3d')
+
+    # n = 8
+    # xpos = Nodes[n*(nx+1)*(ny+1):(n+1)*(nx+1)*(ny+1),1]#[1,2,3,4,5,6,7,8,9,10]
+    # ypos = Nodes[n*(nx+1)*(ny+1):(n+1)*(nx+1)*(ny+1),2]#[2,3,4,5,1,6,2,1,7,2]
+    # num_elements = len(xpos)
+    # zpos = zeros(num_elements)
+    # dx = ones(num_elements)
+    # dy = ones(num_elements)
+    # dz =  Nodes[n*(nx+1)*(ny+1):(n+1)*(nx+1)*(ny+1),4]
+
+    # ax1.bar3d(xpos, ypos, zpos, dx, dy, dz, color='#00ceaa')
+    # plt.show()
+    #
+
+    NE = (nx*(ny+1)+ny*(nx+1)+(nx+1)*(ny+1))*nz
+    Elems = zeros((NE,4))
+    # Creando las conexiones de los elementos verticales
+    c = 0
+    for i in range(nz):
+        for j in range(ny+1):
+            for k in range(nx+1):
+                Elems[c] = [c,c,c+(nx+1)*(ny+1),1]
+                c = c + 1
+    # Creando las conexiones de los elementos horizontales
+    m = (nx+1)*(ny+1)
+    for i in range(nz):
+        for j in range(ny+1):
+            for k in range(nx):
+                Elems[c] = [c,m,m+1,2]
+                m = m + 1
+                c = c + 1
+            m = m + 1
+    # Creando las conexiones de los elementos horizontales
+    n = 0 
+    for i in range(nz):
+        n = n + (nx+1)*(ny+1)
+        for j in range(nx+1):
+            for k in range(ny):
+                Elems[c] = [c,j+k*(nx+1)+n,j+nx+1+k*(nx+1)+n,2]
+                c = c + 1
+    # Creando centro de diafragmas
+    Diap = zeros((nz,4))
+    for i in range(nz):
+        Diap[i] = [i+1000,Lx/2.0,Ly/2.0,h*(i+1)]
+    #
+    return Nodes, Elems, Diap
+
+def espectro_E030(T,Z=0.45,U=1.5,S=1.0,Tp=0.4,Tl=2.5,R=1):
+    from numpy import zeros
+    n = len(T)
+    E030 = zeros(n)
+    for i in range(n):
+        if T[i]>=0 and T[i]<0.2*Tp:
+            E030[i]=2.5#1+7.5*T[i]/Tp
+        elif T[i]>=0.2*Tp and T[i]<Tp:
+            E030[i]=2.5
+        elif T[i]>=Tp and T[i]<Tl:
+            E030[i] = 2.5*(Tp/T[i])
+        elif T[i]>=Tl:
+            E030[i] = 2.5*(Tp*Tl/T[i]**2)
+        else:
+            print("El periodo no puede ser negativo!")
+    return E030*Z*U*S/R
+
+def get_static_loads(coef,p,h,T):
+    from numpy import zeros
+    n = len(h)
+    V = coef*sum(p)
+    F = zeros(n)
+    #
+    if T > 0.0 and T <= 0.5:
+        k=1.0
+    elif T>0.5:
+        k = 0.75+0.5*T
+    else:
+        print('El periodo es negativo!')
+    #
+    div = 0.
+    for i in range(n):
+        div = div + p[i]*h[i]**k
+    #
+    for i in range(n):
+        F[i] = p[i]*h[i]**k/div*V
+    return F,k
+
+def getCombo(E030,MF,modo,NT,Tmodes):
+    import numpy as np
+    import pandas as pd
+    # Obtenemos las Masas totales
+    Mx = sum(sum(MF[0::3,0::3]))
+    My = sum(sum(MF[1::3,1::3]))
+    Mr = sum(sum(MF[2::3,2::3]))
+
+    # Definimos valores iniciales
+    Ux,Uy,Rz = np.zeros(NT),np.zeros(NT),np.zeros(NT)
+    Ux[0::3]=1
+    Uy[1::3]=1
+    Rz[2::3]=1
+    SUMx, SUMy, SUMr = 0., 0., 0.
+    Nmodes = len(modo) 
+
+    # Obtención de Masas Participativas
+    ni=0
+    np.set_printoptions(precision = 4)
+    df1 = pd.DataFrame(columns=['Modo','T(s)','SumUx','SumUy','SumRz'])
+    for j in range(1,Nmodes+1):
+        FPx=modo[j-1].T@MF@Ux
+        FPy=modo[j-1].T@MF@Uy
+        FPr=modo[j-1].T@MF@Rz
+        FPRx=FPx**2/Mx
+        FPRy=FPy**2/My
+        FPRr=FPr**2/Mr
+        SUMx = SUMx + FPRx
+        SUMy = SUMy + FPRy
+        SUMr = SUMr + FPRr
+        #
+        if min(SUMx,SUMy,SUMr)>0.90 and ni==0:
+            ni = j
+        df1 = df1.append({'Modo':j, 'T(s)':Tmodes[j-1],'SumUx':SUMx,
+                        'SumUy':SUMy,'SumRz':SUMr}, ignore_index=True)
+    print('N° mínimo de Modos a considerar:',ni)
+
+    # Definimos valores iniciales
+    D_ABSx,D_RCSCx = np.zeros(NT),np.zeros(NT)
+    Δ_ABSx,Δ_RCSCx = np.zeros(NT),np.zeros(NT)
+    V_ABSx,V_RCSCx = np.zeros(NT),np.zeros(NT)
+    D_ABSy,D_RCSCy = np.zeros(NT),np.zeros(NT)
+    Δ_ABSy,Δ_RCSCy = np.zeros(NT),np.zeros(NT)
+    V_ABSy,V_RCSCy = np.zeros(NT),np.zeros(NT)
+
+    # Se realiza la Superpocisión Modal Espectral
+    for j in range(1,ni+1):#ni+1
+        FPx=modo[j-1].T@MF@Ux
+        FPy=modo[j-1].T@MF@Uy
+        FPr=modo[j-1].T@MF@Rz
+        #
+        Sa = E030[j-1]
+        Sd = Sa*9.81/(2*np.pi/Tmodes[j-1])**2
+        #
+        respDX = Sd*FPx*modo[j-1]
+        respAX = Sa*FPx*MF@modo[j-1]
+        D_ABSx = D_ABSx + abs(respDX)
+        D_RCSCx = D_RCSCx + (respDX)**2
+        respDX[3:] = respDX[3:] - respDX[:-3]
+        Δ_ABSx = Δ_ABSx + abs(respDX)
+        Δ_RCSCx = Δ_RCSCx + (respDX)**2
+        V_ABSx = V_ABSx + abs(np.cumsum(respAX[::-1])[::-1])
+        V_RCSCx = V_RCSCx + (np.cumsum(respAX[::-1])[::-1])**2
+        #
+        respDY = Sd*FPy*modo[j-1]
+        respAY = Sa*FPy*MF@modo[j-1]
+        D_ABSy = D_ABSy + abs(respDY)
+        D_RCSCy = D_RCSCy + (respDY)**2
+        respDY[3:] = respDY[3:] - respDY[:-3]
+        Δ_ABSy = Δ_ABSy + abs(respDY)
+        Δ_RCSCy = Δ_RCSCy + (respDY)**2
+        V_ABSy = V_ABSy + abs(np.cumsum(respAY[::-1])[::-1])
+        V_RCSCy = V_RCSCy + (np.cumsum(respAY[::-1])[::-1])**2
+
+    # Se realiza la combinación 25%ABS + 75%RCSC
+    D_RCSCx = D_RCSCx**0.5
+    Δ_RCSCx = Δ_RCSCx**0.5
+    V_RCSCx = V_RCSCx**0.5
+    DDx = 0.25*D_ABSx + 0.75*D_RCSCx
+    ΔDx = 0.25*Δ_ABSx + 0.75*Δ_RCSCx
+    VDx = 0.25*V_ABSx + 0.75*V_RCSCx
+    #
+    D_RCSCy = D_RCSCy**0.5
+    Δ_RCSCy = Δ_RCSCy**0.5
+    V_RCSCy = V_RCSCy**0.5
+    DDy = 0.25*D_ABSy + 0.75*D_RCSCy
+    ΔDy = 0.25*Δ_ABSy + 0.75*Δ_RCSCy
+    VDy = 0.25*V_ABSy + 0.75*V_RCSCy
+    
+    df2 = pd.DataFrame(columns=['Nivel','VDx(ton)','VDy(ton)','UDx(cm)','UDy(cm)'])
+    for i in range(int(NT/3)):
+        df2 = df2.append({'Nivel':i+1, 'VDx(ton)':VDx[0::3][i]/1000,
+        'VDy(ton)':VDy[1::3][i]/1000,'UDx(cm)':DDx[0::3][i]*100,
+        'UDy(cm)':DDy[1::3][i]*100}, ignore_index=True)
+
+    return DDx, ΔDx, VDx, DDy, ΔDy, VDy, df1.iloc[:ni,:], df2
+
+def genReport(df1,df2,df3,df4,df5,texto1,texto2):
+    from PIL import Image
+    import glob
+    #
+    lista = glob.glob('Mod*.png')
+    #
+    for archivo in lista:
+        im = Image.open(archivo)
+        width, height = im.size
+        left, top = width/6, height/6
+        right, bottom = 9*width/10, 9*height/10
+        im1 = im.crop((left, top, right, bottom))
+        im1.save(archivo)
+    #
+    from docx import Document
+    from docx.shared import Inches
+    from docx.enum.text import WD_ALIGN_PARAGRAPH
+
+    document = Document()
+    title1 = document.add_heading('Informe del Análisis Sísmico', 0)
+    title1.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    p = document.add_paragraph('Realizado por ')
+    p.add_run('JPI Ingeniería e Innovación SAC').bold = True
+    p.add_run(' para el curso ')
+    p.add_run('ASEP.').italic = True
+
+    document.add_paragraph('Edificio Analizado - vista 3D:')
+    document.add_picture('Modelo_3D.png', width=Inches(5.0))
+    document.add_paragraph('Edificación de Categoría Tipo C.')
+
+    document.add_heading('Generalidades', level=1)
+    document.add_paragraph('Metrado de Cargas', style='Intense Quote')
+
+    document.add_paragraph('Para el metrado de cargas se consideró las siguientes cargas distribuidas:')
+    document.add_paragraph('Carga Viva:\t\t\t\t250 kg/m2', style='List Bullet')
+    document.add_paragraph('Carga de Losa:\t\t\t300 kg/m2', style='List Bullet')
+    document.add_paragraph('Carga de Acabados:\t\t100 kg/m2', style='List Bullet')
+    document.add_paragraph('Carga de Tabiquería:\t\t150 kg/m2', style='List Bullet')
+
+    document.add_picture('Modelo_numerico.png', width=Inches(5.0))
+    f1 = document.add_paragraph('Figura 1: Modelo Numérico para el Análisis.')
+    f1.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    document.add_heading('Análisis Modal', level=1)
+    document.add_paragraph('Modos de Vibración', style='Intense Quote')
+    #
+    t1 = document.add_paragraph('\nTabla 1: Factor de Participación de Masas.')
+    t1.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    table1 = document.add_table(rows=df1.shape[0]+1, cols=df1.shape[1])
+    table1.style = 'Light Grid Accent 1'
+    for j in range(df1.shape[-1]):
+        table1.cell(0,j).text = df1.columns[j]
+    for i in range(df1.shape[0]):
+        for j in range(df1.shape[-1]):
+            table1.cell(i+1,j).text = str(df1.values[i,j].round(4))
+
+    document.add_picture('Modo_1.png', width=Inches(5.0))
+    f2 = document.add_paragraph('Figura 2: Primer modo de vibración.')
+    f2.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    document.add_picture('Modo_2.png', width=Inches(5.0))
+    f3 = document.add_paragraph('Figura 3: Segundo modo de vibración.')
+    f3.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    document.add_picture('Modo_3.png', width=Inches(5.0))
+    f4 = document.add_paragraph('Figura 4: Tercer modo de vibración.')
+    f4.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    #
+    document.add_heading('Análisis Sísmico', level=1)
+    document.add_paragraph('Análisis Estático', style='Intense Quote')
+    p1 = document.add_paragraph(texto1)
+    p1.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+    #
+    t2 = document.add_paragraph('\nTabla 2: Fuerzas y desplazamientos del análisis estático en X.')
+    t2.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    table3 = document.add_table(rows=df3.shape[0]+1, cols=df3.shape[1])
+    table3.style = 'Light Grid Accent 1'
+    for j in range(df3.shape[-1]):
+        table3.cell(0,j).text = df3.columns[j]
+    for i in range(df3.shape[0]):
+        for j in range(df3.shape[-1]):
+            table3.cell(i+1,j).text = str(df3.values[i,j].round(4))
+    t3 = document.add_paragraph('\nTabla 3: Fuerzas y desplazamientos del análisis estático en Y.')
+    t3.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    #
+    table4 = document.add_table(rows=df4.shape[0]+1, cols=df4.shape[1])
+    table4.style = 'Light Grid Accent 1'
+    for j in range(df4.shape[-1]):
+        table4.cell(0,j).text = df4.columns[j]
+    for i in range(df4.shape[0]):
+        for j in range(df4.shape[-1]):
+            table4.cell(i+1,j).text = str(df4.values[i,j].round(4))
+    #
+    document.add_paragraph('Análisis Dinámico Modal Espectral', style='Intense Quote')
+    #
+    document.add_paragraph('En este análisis se consideraron los siguientes parámetros sísmicos:')
+    document.add_paragraph('Factor de Zona:\t\t\t\tZ = 0.45', style='List Bullet')
+    document.add_paragraph('Factor de Uso:\t\t\t\tU = 1.00', style='List Bullet')
+    document.add_paragraph('F. de Amplificación del Suelo:\t\tS = 1.00', style='List Bullet')
+    document.add_paragraph('Coef. de Reducción:\t\t\tRo= 8.00', style='List Bullet')
+
+    document.add_picture('Espectro_E030.png', width=Inches(5.4))
+    f5 = document.add_paragraph('Figura 5: Espectro según la norma E030.')
+    f5.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    t4 = document.add_paragraph('\nTabla 4: Respuesta Dinámica sin escalar.')
+    t4.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    table2 = document.add_table(rows=df2.shape[0]+1, cols=df2.shape[1])
+    table2.style = 'Light Grid Accent 1'
+    for j in range(df2.shape[-1]):
+        table2.cell(0,j).text = df2.columns[j]
+    for i in range(df2.shape[0]):
+        for j in range(df2.shape[-1]):
+            table2.cell(i+1,j).text = str(df2.values[i,j].round(4))
+    #
+    p2 = document.add_paragraph(texto2)
+    p2.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+    #
+    t5 = document.add_paragraph('\nTabla 5: Respuesta Dinámica Escalada.')
+    t5.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    table5 = document.add_table(rows=df5.shape[0]+1, cols=df5.shape[1])
+    table5.style = 'Light Grid Accent 1'
+    for j in range(df5.shape[-1]):
+        table5.cell(0,j).text = df5.columns[j]
+    for i in range(df5.shape[0]):
+        for j in range(df5.shape[-1]):
+            table5.cell(i+1,j).text = str(df5.values[i,j].round(4))
+    #
+    document.add_page_break()
+    document.add_heading('Resultados', level=1)
+    document.add_paragraph('Distorsiones de Entrepiso', style='Intense Quote')
+    document.add_picture('distorsion_din.png', width=Inches(5.0))
+    f6 = document.add_paragraph('Figura 6: Distorsión de entrepiso del análisis dinámico.')
+    f6.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    file = 'Informe de Analisis Sismico.docx'
+    document.save(file)
+    import os
+    os.startfile('%s'%file)
